3c316a2a7bd9868dee52035670d93583bb135888
John LaFata <johnlafata61@gmail.com>
2025-05-28 17:49:32 -0400

    adds scripts

---
+++ jumphost-scripts/backup-config.sh
@@ -0,0 +1,6 @@
+export OM_ENV=/home/ubuntu/om-cli-env.yml
+mkdir sandbox
+om -k --env=$OM_ENV staged-director-config --no-redact >sandbox-backup/director-config.yml 
+om -k --env=$OM_ENV staged-config -p cf >sandbox-backup/cf-config.yml 
+om -k --env=$OM_ENV staged-config -p metric-store >sandbox-backup/metric-store-config.yml
+om -k --env=$OM_ENV staged-config -p credhub-service-broker >sandbox-backup/credhub-service-broker-config.yml
---
+++ opsmgr-scripts/stop-deployments.sh
@@ -0,0 +1,4 @@
+bosh stop -d appMetrics-5ecb8a524d3dfdf56402  --hard
+bosh stop -d metric-store-a5ba46c090b35dba5def --hard
+bosh stop -d credhub-service-broker-b0f8dc1e607e78c3e530 --hard
+bosh stop -d cf-4b418c8da64b89bf651a --hard
