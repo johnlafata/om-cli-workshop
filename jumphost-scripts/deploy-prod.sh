36932fbacfc6cd50fdebb4ac97b58d937c22677e
John LaFata <johnlafata61@gmail.com>
2025-05-29 01:22:57 +0000

    corrects permissions and scripts

--- jumphost-scripts/deploy-prod.sh
+++ jumphost-scripts/deploy-prod.sh
@@ -2,6 +2,6 @@ export OM_ENV=om-cli-prod-env.yml
 PROD_VCENTER_PASSWORD=$(cat prod_vcenter_password.txt) \
   om interpolate \
   --config ../parameterized/director-config.yml \
-  --vars-env PROD
+  --vars-env PROD \
   --vars-file=../parameterized/director-vars.yml
 
