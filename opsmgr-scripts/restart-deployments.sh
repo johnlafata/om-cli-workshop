717b505ee28c03799b40dbbaa116683f5581f543
John LaFata <johnlafata61@gmail.com>
2025-06-01 18:56:25 +0000

    remove prompts from bosh stop and restart

--- opsmgr-scripts/restart-deployments.sh
+++ opsmgr-scripts/restart-deployments.sh
@@ -1,4 +1,4 @@
-bosh start -d cf-4b418c8da64b89bf651a 
-bosh start -d credhub-service-broker-b0f8dc1e607e78c3e530 
-bosh start -d metric-store-a5ba46c090b35dba5def 
-bosh start -d appMetrics-5ecb8a524d3dfdf56402  
+bosh start -d cf-4b418c8da64b89bf651a -n
+bosh start -d credhub-service-broker-b0f8dc1e607e78c3e530 -n
+bosh start -d metric-store-a5ba46c090b35dba5def -n
+bosh start -d appMetrics-5ecb8a524d3dfdf56402 -n 
