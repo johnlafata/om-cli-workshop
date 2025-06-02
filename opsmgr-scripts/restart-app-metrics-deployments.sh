c4a194ac835e4ab2a2299b93e61304089246798a
John LaFata <johnlafata61@gmail.com>
2025-06-02 10:31:09 -0400

    scripts app metrics deployment management

---
+++ opsmgr-scripts/restart-app-metrics-deployments.sh
@@ -0,0 +1,2 @@
+bosh start -d metric-store-a5ba46c090b35dba5def -n
+bosh start -d appMetrics-5ecb8a524d3dfdf56402 -n 
---
+++ opsmgr-scripts/stop-app-metrics-deployments.sh
@@ -0,0 +1,2 @@
+bosh stop -d appMetrics-5ecb8a524d3dfdf56402  --hard -n
+bosh stop -d metric-store-a5ba46c090b35dba5def --hard -n
