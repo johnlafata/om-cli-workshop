3c0d5474f8827ddf0237f12c887e6ee997bc652d
John LaFata <johnlafata61@gmail.com>
2025-06-02 16:27:42 +0000

    adds scripts to stop and restart healthwatch deployments

---
+++ opsmgr-scripts/restart-healthwatch-depoyments.sh
@@ -0,0 +1,2 @@
+bosh start -d p-healthwatch2-68bc20142a56c776dff8 -n
+bosh start -d p-healthwatch2-pas-exporter-537de159669302780b02 -n 
---
+++ opsmgr-scripts/stop-healthwatch-deployments.sh
@@ -0,0 +1,2 @@
+bosh stop -d p-healthwatch2-pas-exporter-537de159669302780b02 --hard -n
+bosh stop -d p-healthwatch2-68bc20142a56c776dff8 --hard -n
