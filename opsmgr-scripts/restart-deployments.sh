1b14238998ca33cee164646c01ea2ffc1a1ff9de
John LaFata <johnlafata61@gmail.com>
2025-06-02 19:38:32 -0400

    includes healthwatch deployments in stop and restart scripts

--- opsmgr-scripts/restart-deployments.sh
+++ opsmgr-scripts/restart-deployments.sh
@@ -2,3 +2,5 @@ bosh start -d cf-4b418c8da64b89bf651a -n
 bosh start -d credhub-service-broker-b0f8dc1e607e78c3e530 -n
 bosh start -d metric-store-a5ba46c090b35dba5def -n
 bosh start -d appMetrics-5ecb8a524d3dfdf56402 -n 
+bosh start -d p-healthwatch2-68bc20142a56c776dff8 -n
+bosh start -d p-healthwatch2-pas-exporter-537de159669302780b02 -n 
--- opsmgr-scripts/stop-deployments.sh
+++ opsmgr-scripts/stop-deployments.sh
@@ -1,3 +1,5 @@
+bosh stop -d p-healthwatch2-pas-exporter-537de159669302780b02 --hard -n
+bosh stop -d p-healthwatch2-68bc20142a56c776dff8 --hard -n
 bosh stop -d appMetrics-5ecb8a524d3dfdf56402  --hard -n
 bosh stop -d metric-store-a5ba46c090b35dba5def --hard -n
 bosh stop -d credhub-service-broker-b0f8dc1e607e78c3e530 --hard -n
